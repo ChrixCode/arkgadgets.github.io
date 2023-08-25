1a35ef800d8ae898b9841441f7111a0b041e109a
ChrixCode <116160045+ChrixCode@users.noreply.github.com>
2023-08-25 12:42:48 +0100

    Add files via upload

---
+++ branch/socials.php
@@ -0,0 +1,71 @@
+<hr>
+    
+    <section id="instagram" class="padding-large">
+      <div class="container">
+        <div class="section-header">
+          <h2 class="section-title">Follow our instagram</h2>
+        </div>
+             <div class="row d-flex flex-wrap">
+          <article class="col-md-4 post-item">
+            <div class="image-holder zoom-effect">
+              <a href="single-post.html">
+                <img src="images/post-img1.jpg" alt="post" class="post-image">
+              </a>
+            </div>
+            <div class="post-content d-flex">
+              <div class="meta-date">
+                <div class="meta-day text-primary">22</div>
+                <div class="meta-month">Aug-2021</div>
+              </div>
+              <div class="post-header">
+                <h3 class="post-title">
+                  <a href="single-post.html">top 10 casual look ideas to dress up your kids</a>
+                </h3>
+                <a href="blog.html" class="blog-categories">Fashion</a>
+              </div>
+            </div>
+          </article>
+          <article class="col-md-4 post-item">
+            <div class="image-holder zoom-effect">
+              <a href="single-post.html">
+                <img src="images/post-img2.jpg" alt="post" class="post-image">
+              </a>
+            </div>
+            <div class="post-content d-flex">
+              <div class="meta-date">
+                <div class="meta-day text-primary">25</div>
+                <div class="meta-month">Aug-2021</div>
+              </div>
+              <div class="post-header">
+                <h3 class="post-title">
+                  <a href="single-post.html">Latest trends of wearing street wears supremely</a>
+                </h3>
+                <a href="blog.html" class="blog-categories">Trending</a>
+              </div>
+            </div>
+          </article>
+          <article class="col-md-4 post-item">
+            <div class="image-holder zoom-effect">
+              <a href="single-post.html">
+                <img src="images/post-img3.jpg" alt="post" class="post-image">
+              </a>
+            </div>
+            <div class="post-content d-flex">
+              <div class="meta-date">
+                <div class="meta-day text-primary">28</div>
+                <div class="meta-month">Aug-2021</div>
+              </div>
+              <div class="post-header">
+                <h3 class="post-title">
+                  <a href="single-post.html">types of comfortable clothes ideas for women</a>
+                </h3>
+                <a href="blog.html" class="blog-categories">Inspiration</a>
+              </div>
+            </div>
+          </article>
+          
+        </div>
+
+      </div>
+      <hr>
+    </section>
---
+++ branch/top.php
@@ -0,0 +1,145 @@
+<!DOCTYPE php>
+<php lang="en">
+  <head>
+    <title>Ultras - Clothing Store eCommerce Store HTML Website Template</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="icomoon/icomoon.css">
+    <link rel="stylesheet" type="text/css" media="all" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/vendor.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+  </head>
+  <body>
+
+    <div class="preloader-wrapper">
+      <div class="preloader">
+      </div>
+    </div>
+
+    <div class="search-popup">
+      <div class="search-popup-container">
+
+        <form role="search" method="get" class="search-form" action="">
+          <input type="search" id="search-form" class="search-field" placeholder="Type and press enter" value="" name="s" />
+          <button type="submit" class="search-submit"><a href="#"><i class="icon icon-search"></i></a></button>
+        </form>
+
+        <h5 class="cat-list-title">Browse Categories</h5>
+        
+        <ul class="cat-list">
+          <li class="cat-list-item">
+            <a href="shop.php" title="Men Jackets">Men Jackets</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Fashion">Fashion</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Casual Wears">Casual Wears</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Women">Women</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Trending">Trending</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Hoodie">Hoodie</a>
+          </li>
+          <li class="cat-list-item">
+            <a href="shop.php" title="Kids">Kids</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <header id="header" >
+      <div id="header-wrap">
+        <nav class="secondary-nav border-bottom">
+          <div class="container">
+            <div class="row d-flex align-items-center">
+              <div class="col-md-4 header-contact">
+                <p>Let's talk! <strong>+57 444 11 00 35</strong>
+                </p>
+              </div>
+              <div class="col-md-4 shipping-purchase text-center">
+                <p>Free shipping on a purchase value of $200</p>
+              </div>
+              <div class="col-md-4 col-sm-12 user-items">
+                <ul class="d-flex justify-content-end list-unstyled">
+                  <li class="user-items search-item pe-2">
+                    <a href="#" class="search-button">
+                      <i class="icon icon-search"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <nav class="primary-nav padding-small">
+          <div class="container fixed-top">
+            <div class="row d-flex align-items-center">
+              <div class="col-lg-2 col-md-2">
+                <div class="main-logo">
+                  <a href="index.php">
+                    <img src="images/sapphireslogo.png" class="img-responsive" width="15%" alt="logo">
+                    <span class="p-2"><b >Sapphires451</b></p>
+                  </a>
+                </div>
+              </div>
+              <div class="col-lg-10 col-md-10">
+                <div class="navbar">
+
+                  <div id="main-nav" class="stellarnav d-flex justify-content-end right">
+                    <ul class="menu-list">
+
+                      <li class="menu-item has-sub">
+                        <a href="index.php" class="item-anchor d-flex align-item-center" data-effect="Home">Home</a
+                      </li>
+
+                      <li><a href="about.php" class="item-anchor" data-effect="About">About</a></li>
+
+                      <li class="menu-item">
+                        <a href="shop.php" class="item-anchor d-flex align-item-center" data-effect="Shop">Shop</a>
+                      </li>
+
+                      <li class="menu-item">
+                        <a href="styles.php" class="item-anchor d-flex align-item-center" data-effect="Pages">Pages<i class="icon icon-chevron-down"></i></a>
+                        <ul class="submenu">
+                          <li><a href="styles.php" class="item-anchor">Styles</a></li>
+                          <li><a href="thank-you.php" class="item-anchor">Thankyou</a></li>
+                        </ul>
+                      </li>
+
+                      <li class="menu-item has-sub">
+                        <a href="blog.php" class="item-anchor d-flex align-item-center" data-effect="Blog">Blog<i class="icon icon-chevron-down"></i></a>
+                        <ul class="submenu">
+                          <li><a href="blog.php" class="item-anchor">Blog</a></li>
+                          <li><a href="single-post.php" class="item-anchor">Single post</a></li>
+                        </ul>
+                      </li>
+
+                      <li><a href="contact.php" class="item-anchor" data-effect="Contact">Contact</a></li>
+
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
