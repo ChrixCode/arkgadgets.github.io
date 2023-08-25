f73d0f5e6e40f15016504aeeb6bf368b20a48235
ChrixCode <116160045+ChrixCode@users.noreply.github.com>
2023-08-25 12:38:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,368 @@
+<?php
+
+    include('branch/top.php');
+
+?>
+
+    <section id="billboard" class="overflow-hidden">
+
+      <button class="button-prev">
+        <i class="icon icon-chevron-left"></i>
+      </button>
+      <button class="button-next">
+        <i class="icon icon-chevron-right"></i>
+      </button>
+      <div class="swiper main-swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" style="background-image: url('images/banner1.png');background-repeat: no-repeat;background-size: cover;background-position: center;">
+            <div class="banner-content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-5">
+                    <h2 class="banner-title">Summer Collection</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
+                    <div class="btn-wrap">
+                      <a href="shop.php" class="btn btn-light btn-medium d-flex align-items-center" tabindex="0">Shop it now <i class="icon icon-arrow-io"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="background-image: url('images/MainBG.jpg');background-repeat: no-repeat;background-size: cover;background-position: center;">
+            <div class="banner-content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-5">
+                    <h2 class="banner-title">Casual Collection</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
+                    <div class="btn-wrap">
+                      <a href="shop.php" class="btn btn-light btn-light-arrow btn-medium d-flex align-items-center" tabindex="0">Shop it now <i class="icon icon-arrow-io"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="instagram" class="padding-large">
+      <div class="container">
+        <div class="row d-flex flex-wrap justify-content-between">
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image1.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image2.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image3.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image4.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image5.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6">
+            <figure class="zoom-effect">
+              <img src="images/insta-image6.jpg" alt="instagram" class="insta-image">
+              <i class="icon icon-instagram"></i>
+            </figure>
+          </div>
+        </div>          
+      </div>
+    </section>
+
+
+
+    <section id="subscribe" class="padding-large">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="col-md-12">
+              <h2 class="col-md-8">Get 25% off Discount Coupons</h2>
+            </div>
+            <p class="col-md-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dictumst amet, metus, sit massa posuere maecenas. At tellus ut nunc amet vel egestas.</p>
+          </div>
+          <div class="col-md-12">
+            <form id="form" class="form-control">
+              <input class="form-control" type="text" name="email" placeholder="Enter your email addresss here">
+              <button class="btn btn-dark">Subscribe Now</button>
+            </form>
+          </div>
+
+
+
+
+        </div>
+      </div>
+    </section>
+
+
+
+    <section id="testimonials" class="padding-large no-padding-bottom">
+      <div class="container">
+        <div class="reviews-content">
+          <div class="row d-flex flex-wrap">
+            <div class="col-md-2">
+              <div class="review-icon">
+                <i class="icon icon-right-quote"></i>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="swiper testimonial-swiper overflow-hidden">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <div class="testimonial-detail">
+                      <p>“Dignissim massa diam elementum habitant fames. Id nullam pellentesque nisi, eget cursus dictumst pharetra, sit. Pulvinar laoreet id porttitor egestas dui urna. Porttitor nibh magna dolor ultrices iaculis sit iaculis.”</p>
+                      <div class="author-detail">
+                        <div class="name">By Maggie Rio</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="testimonial-detail">
+                      <p>“Dignissim massa diam elementum habitant fames. Id nullam pellentesque nisi, eget cursus dictumst pharetra, sit. Pulvinar laoreet id porttitor egestas dui urna. Porttitor nibh magna dolor ultrices iaculis sit iaculis.”</p>
+                      <div class="author-detail">
+                        <div class="name">By John Smith</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-arrows">
+                <button class="prev-button">
+                  <i class="icon icon-arrow-left"></i>
+                </button>
+                <button class="next-button">
+                  <i class="icon icon-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="flash-sales" class="product-store padding-large">
+      
+      <div class="container">
+        <div class="section-header">
+          <h2 class="section-title">Flash sales</h2>
+        </div>
+        <div class="swiper product-swiper flash-sales overflow-hidden">
+
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="product-item">
+                <img src="images/selling-products9.jpg" alt="Books" class="product-image">
+                <div class="cart-concern">
+                  <div class="cart-button d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn-wrap cart-link d-flex align-items-center">add to cart <i class="icon icon-arrow-io"></i>
+                    </button>
+                    <button type="button" class="view-btn tooltip
+                        d-flex">
+                      <i class="icon icon-screen-full"></i>
+                      <span class="tooltip-text">Quick view</span>
+                    </button>
+                    <button type="button" class="wishlist-btn">
+                      <i class="icon icon-heart"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="discount">10% Off</div>
+                <div class="product-detail">
+                  <h3 class="product-title">
+                    <a href="single-product.php">Full sleeve cover shirt</a>
+                  </h3>
+                  <div class="item-price text-primary">
+                    <del class="prev-price">$50.00</del>$40.00
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="product-item">
+                <img src="images/selling-products10.jpg" alt="Books" class="product-image">
+                <div class="cart-concern">
+                  <div class="cart-button d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn-wrap cart-link d-flex align-items-center">add to cart <i class="icon icon-arrow-io"></i>
+                    </button>
+                    <button type="button" class="view-btn tooltip
+                        d-flex">
+                      <i class="icon icon-screen-full"></i>
+                      <span class="tooltip-text">Quick view</span>
+                    </button>
+                    <button type="button" class="wishlist-btn">
+                      <i class="icon icon-heart"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="discount">10% Off</div>
+                <div class="product-detail">
+                  <h3 class="product-title">
+                    <a href="single-product.php">Long Sleeve T-shirt</a>
+                  </h3>
+                  <div class="item-price text-primary">
+                    <del class="prev-price">$50.00</del>$40.00
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="product-item">
+                <img src="images/selling-products11.jpg" alt="Books" class="product-image">
+                <div class="cart-concern">
+                  <div class="cart-button d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn-wrap cart-link d-flex align-items-center">add to cart <i class="icon icon-arrow-io"></i>
+                    </button>
+                    <button type="button" class="view-btn tooltip
+                        d-flex">
+                      <i class="icon icon-screen-full"></i>
+                      <span class="tooltip-text">Quick view</span>
+                    </button>
+                    <button type="button" class="wishlist-btn">
+                      <i class="icon icon-heart"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="discount">10% Off</div>
+                <div class="product-detail">
+                  <h3 class="product-title">
+                    <a href="single-product.php">Grey Check Coat</a>
+                  </h3>
+                  <div class="item-price text-primary">
+                    <del class="prev-price">$55.00</del>$45.00
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="product-item">
+                <img src="images/selling-products12.jpg" alt="Books" class="product-image">
+                <div class="cart-concern">
+                  <div class="cart-button d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn-wrap cart-link d-flex align-items-center">add to cart <i class="icon icon-arrow-io"></i>
+                    </button>
+                    <button type="button" class="view-btn tooltip
+                        d-flex">
+                      <i class="icon icon-screen-full"></i>
+                      <span class="tooltip-text">Quick view</span>
+                    </button>
+                    <button type="button" class="wishlist-btn">
+                      <i class="icon icon-heart"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="discount">10% Off</div>
+                <div class="product-detail">
+                  <h3 class="product-title">
+                    <a href="single-product.php">Silk White Shirt</a>
+                  </h3>
+                  <div class="item-price text-primary">
+                    <del class="prev-price">$45.00</del>$35.00
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="product-item">
+                <img src="images/selling-products8.jpg" alt="Books" class="product-image">
+                <div class="cart-concern">
+                  <div class="cart-button d-flex justify-content-between align-items-center">
+                    <button type="button" class="btn-wrap cart-link d-flex align-items-center">add to cart <i class="icon icon-arrow-io"></i>
+                    </button>
+                    <button type="button" class="view-btn tooltip
+                        d-flex">
+                      <i class="icon icon-screen-full"></i>
+                      <span class="tooltip-text">Quick view</span>
+                    </button>
+                    <button type="button" class="wishlist-btn">
+                      <i class="icon icon-heart"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="discount">10% Off</div>
+                <div class="product-detail">
+                  <h3 class="product-title">
+                    <a href="single-product.php">Blue Jeans pant</a>
+                  </h3>
+                  <div class="item-price text-primary">
+                    <del class="prev-price">$45.00</del>$35.00
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="shoppify-section-banner">
+      <div class="container">
+        <div class="product-collection">
+          <div class="left-content collection-item">
+            <div class="products-thumb">
+              <img src="images/model.png" alt="collection item" class="large-image image-rounded">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 product-entry">
+              <div class="categories">Denim collection</div>
+              <h3 class="item-title">The casual selection.</h3>
+              <p>Vel non viverra ligula odio ornare turpis mauris. Odio aliquam, tincidunt ut vitae elit risus. Tempor egestas condimentum et ac rutrum dui, odio.</p>
+              <div class="btn-wrap">
+                <a href="shop.php" class="d-flex align-items-center">shop collection <i class="icon icon-arrow-io"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>        
+      </div>
+    </section>
+
+    <section id="quotation" class="align-center padding-large">
+      <div class="inner-content">
+        <h2 class="section-title divider">Quote of the day</h2>
+        <blockquote>
+          <q>It's true, I don't like the whole cutoff-shorts-and-T-shirt look, but I think you can look fantastic in casual clothes.</q>
+          <div class="author-name">- Dr. Seuss</div>
+        </blockquote>
+      </div>
+    </section>
+
+  
+<?php
+
+include ('branch/socials.php');
+
+
+?>
+
+
+
+
+<?php
+
+    include ('branch/bottom.php');
+
+
+?>
---
+++ textimg.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+            .firstfade-in{
+                animation: firstfade 2s;
+            }
+            @keyframes firstfade {
+                from {
+                    opacity: 1;
+                }
+                to {
+                    opacity: 0;
+                }
+            }
+
+            .Secondfade-in{
+                animation: Secondfade 4s;
+               
+            }
+            @keyframes Secondfade {
+                from {
+                    opacity: 0 ;
+                }
+                to {
+                    opacity: 2;
+                }
+            }
+
+
+    </style>
+</head>
+<body>
+    
+         <div class="container">
+
+         <div class="outer">
+
+         <div class="details">
+
+         <h1> Changing Image </h1>
+         <h2>
+            <
+
+
+        </div>
+
+         </div>
+            
+        </div>
+
+
+</body>
+</html>
